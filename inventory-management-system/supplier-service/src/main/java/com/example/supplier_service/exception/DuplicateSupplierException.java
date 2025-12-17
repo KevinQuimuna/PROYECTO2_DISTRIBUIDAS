0f1b930b513707176e4c682ddbe3859f7b604a1d
Mishell Estefania Cuasquer Chisaguano <107574737+MishellCuasquer@users.noreply.github.com>
2025-12-17 03:07:16 -0500

    .
    .

--- inventory-management-system/supplier-service/src/main/java/com/example/supplier_service/exception/DuplicateSupplierException.java
+++
@@ -1,7 +0,0 @@
-package com.example.supplier_service.exception;
-
-public class DuplicateSupplierException extends RuntimeException {
-    public DuplicateSupplierException(String message) {
-        super(message);
-    }
-}
--- inventory-management-system/supplier-service/src/main/java/com/example/supplier_service/exception/SupplierNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.supplier_service.exception;
-
-
-public class SupplierNotFoundException extends RuntimeException {
-    public SupplierNotFoundException(String message) {
-        super(message);
-    }
-}
